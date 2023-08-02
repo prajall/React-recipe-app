096b663e306a0f6d785a3fc601274950240bab97
Prajal Maharjan <91400741+prajall@users.noreply.github.com>
2023-08-02 23:36:18 +0545

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "../components/Logo";
 import Search from "../components/Search";
 import Pages from "./Pages";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 
-const Home = () => {
   useEffect(() => {
     document.title = "Chef's Recipes";
   }, []);
+const Home = () => {
   return (
     <div className="my-10 md:my-5 lg:my-0">
       <Hero />
